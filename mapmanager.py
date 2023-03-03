fe927fa05521f1d636e1f122ba57199a0bda4f0e
RasulAuthor <114753094+RasulDzH@users.noreply.github.com>
2023-03-03 21:19:47 +0600

    Add files via upload

---
+++ mapmanager.py
@@ -0,0 +1,69 @@
+class Mapmanager():
+    """ Управление картой """
+    def __init__(self):
+        self.model = 'block'
+        self.texture = 'block.png'
+        self.block = loader.loadModel(self.model)
+        self.block.setTexture(loader.loadTexture(self.texture))
+        self.color = (0.2, 0.2, 0.35, 1)
+        
+        self.startNew()
+        self.addBlock((0, 10, 0))
+    def startNew(self):
+        """ создаёт основу для новой карты """
+        self.land = render.attachNewNode("Land")
+    
+    def addBlock(self, position):
+        self.block = loader.loadModel(self.model)
+        self.block.setTexture(loader.loadTexture(self.texture))
+        self.block.setPos(position)
+        self.block.setColor(self.color)
+        self.block.reparentTo(self.land)
+        self.block.setTag("at", str(position))
+    
+    def clear(self):
+        self.land.removeNode()
+        self.startNew()
+    
+    def LoadLand(self, filename):
+        self.clear()
+        with open(filename) as file:
+            y = 0
+            for line in file:
+                x = 0
+                line = line.split(' ')
+                for z in line:
+                    for z0 in range(int(z)+1):
+                        block = self.addBlock((x, y, z0))
+                    x += 1
+                y += 1
+                
+    def findBlocks(self, pos):
+         return self.land.findAllMatches("=at=" + str(pos))
+    def isEmpty(self, pos):
+        blocks = self.findBlocks(pos)
+        if blocks:
+            return False
+        else:
+            return True
+    def findHighestEmpty(self, pos):
+        x, y, z = pos
+        z = 1
+        while not self.isEmpty((x, y, z)):
+            z += 1
+        return(x, y, z)
+    def buildBlock(self, pos):
+        x, y, zz = pos
+        new = self.findHighestEmpty(pos)
+        if new[2] <= z + 1:
+            self.addBlock(new)
+    def delBlock(self, position):
+        bocks = self.findBlocks(position)
+        for block in blocks:
+            block.removeNode()
+    def delBlockFrom(self, position):
+        x, y, z = self.findHighestEmpty(position)
+        pos = x, y, z - 1
+        for block in self.findBlocks(pos):
+            block.removeNode()
+    
